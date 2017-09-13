a40ca1c7057c723e732731a7e8fe57d656c1ce1c
Kolman <ZKOLMAN@depaul.edu>
2017-09-13 16:27:17 -0500

    We now have a demo scene with a Tentacruel and a Metapod

---
+++ Assets/Scripts/tentaRise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tentaRise : MonoBehaviour {
+
+	public GameObject Tentacruel;
+	public Animator TentaAnim;
+	// Use this for initialization
+	void Start () {
+		
+		TentaAnim = Tentacruel.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Boat") {
+			Debug.Log ("Here it is");
+			TentaAnim.SetBool ("isColliding", true);
+		}
+	}
+}
