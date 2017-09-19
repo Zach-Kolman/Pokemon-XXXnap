4fd6a9241d96b50e259edd99f11363e060f6c9c2
Jmotastic117 <jmotto19@gmail.com>
2017-09-19 17:50:34 -0500

    Set up a basic Raycast and implemented a Crosshair color change that will still need some work

---
+++ Assets/Scripts/CrossHairColor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrossHairColor : MonoBehaviour {
+
+   
+    public RawImage crosshair;
+   // public Color newColor;
+
+    // Use this for initialization
+    void Start () {
+        crosshair = GetComponent<RawImage>();
+       
+        //newColor = GetComponent<Color>();
+    }
+	
+	// Update is called once per frame
+	public void Update () {
+        
+
+    }
+
+    public void ChangeColor()
+    {
+        crosshair.color = Color.red;
+
+
+
+
+    }
+
+    public void ChangeWhite()
+    {
+
+        crosshair.color = Color.white;
+
+
+    }
+}
---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour {
+
+    Camera ourCam;
+    CrossHairColor colorCross;
+   public RawImage crosshair;
+    public Color newColor;
+    bool isOnTarget = false;
+
+	// Use this for initialization
+	void Start () {
+        crosshair = GetComponent<RawImage>();
+        ourCam = GetComponent<Camera>();
+        colorCross = FindObjectOfType<CrossHairColor>();
+       
+       
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        Ray ray = ourCam.ScreenPointToRay(new Vector3(340, 200, 0));
+        Debug.DrawRay(ray.origin, ray.direction, Color.green);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+                hit.collider.enabled = false;
+                colorCross.ChangeColor();
+                isOnTarget = true;
+                Debug.Log("You are aiming at POKE");
+            } 
+           
+           
+        }
+
+
+
+    }
+    }
